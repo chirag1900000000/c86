d35e360de151cabe91f4b650d8160c8ade670118
chirag1900000000 <91605337+chirag1900000000@users.noreply.github.com>
2021-12-21 16:10:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+var canvas = new fabric.Canvas("myCanvas");
+ var x= document.getElementById("myAudio");
+ var block_image_object = " ";
+
+function new_image()
+{
+	fabric.Image.fromURL('BirthdayImage.jpg', function(Img){
+        block_image_object = Img;
+
+        block_image_object.scaleToWIdth(700);
+        block_image_object.scaleToHeight(510);
+        block_image_object.set({
+            top:0,
+            left:0
+        });
+        canvas.add(block_image_object);
+    });
+}
+
+function playSound(){
+	x.play();
+}
+
+function new_image(get_image)
+{
+    fabric.Image.fromURL(get_image, function(Img){
+    block_image_object = Img;
+
+    block_image_object.scaleToWidth(block_image_width);
+    block_image_object.scaleToHeight(block_image_height);
+    block_image_object.set({
+    top:player_y,
+    left:player_x
+    });
+    canvas.add(block_image_object);
+    });
+}
